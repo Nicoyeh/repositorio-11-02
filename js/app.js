2dcd17edccce6fe424bdfd1aea9269673122c944
Nicolle Pineda <nicolle15777@gmail.com>
2026-09-07 19:34:01 -0500

    jeje

--- js/app.js
+++ js/app.js
@@ -125,6 +125,7 @@ const DOCUMENTS = [
   { subject:'ingles',      folder:'Exámenes corregidos',                period:'Periodo 3', title:'Examen de Voz pasiva',             file:'assets/pdf/Ingles/Exámenes corregidos/Periodo-3/examen-vozpasiva.pdf'},
   { subject:'ingles',      folder:'Exámenes corregidos',                period:'Periodo 3', title:'Examen de Vocabulario Crime',             file:'assets/pdf/Ingles/Exámenes corregidos/Periodo-3/examen-crime.pdf'},
   { subject: 'calculo', folder: 'Diapositivas', period: 'Periodo 3', title: 'Diapositivas de analisis de limites laterales #2', file: 'assets/pdf/Calculo/Diapositivas/Periodo-3/limites-laterales#2.pdf' },
+  { subject: 'calculo', folder: 'Exámenes corregidos', period: 'Periodo 3', title: 'Examen de limites', file: 'assets/pdf/Calculo/Exámenes corregidos/Periodo-3/examen-limites-corregido.pdf' },
 
   
 ];
